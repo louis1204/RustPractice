34549830726971a8c3587991815c9a45a66d3d8a
Louis Wong <louis.d.wong@oracle.com>
2020-12-30 22:24:21 -0800

    That was rough

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod two_sum;
+// mod two_sum;
+mod first_unique_character_in_string;
 
 fn main() {
-    // let res: str = format!("{:?}", two_sum::Solution.two_sum(vec![2, 2], 4));
-    println!("{:?}", two_sum::Solution::two_sum(vec![2, 2], 4));
+    // println!("{:?}", two_sum::Solution::two_sum(vec![2, 2], 4));
+    println!("{:?}", first_unique_character_in_string::Solution::first_uniq_char(String::from("leetcode")));
 }
